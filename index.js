c71099fa09533fc0fc691e909e99c56f2b0861ee
Handi Pradana <pradana.handi@gmail.com>
2022-06-21 15:24:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -74,6 +74,6 @@ app.post('/delete',(req, res) => {
 });
 
 //server listening
-app.listen(8000, () => {
-  console.log('Server is running at port 8000');
+app.listen(80, () => {
+  console.log('Server is running at port 80');
 });
