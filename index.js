9745e74a34c903db3b79dc479207a33c427eccc0
Handi Pradana <pradana.handi@gmail.com>
2024-07-16 11:18:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,79 +10,102 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const app = express();
 
-//Create Connection
+// Create connection
 require('dotenv/config')
 const conn = mysql.createConnection({
   host: process.env.DB_HOST,
-  user : process.env.DB_USER,
-  password : process.env.DB_PASSWORD,
-  database : process.env.DB_DATABASE
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
-//connect to database
-conn.connect((err) =>{
-  if(err) throw err;
+// Connect to database and create database/tables if they don't exist
+conn.connect((err) => {
+  if (err) throw err;
   console.log('Mysql Connected...');
+  
+  const createDatabase = `CREATE DATABASE IF NOT EXISTS \`${process.env.DB_DATABASE}\`;`;
+  conn.query(createDatabase, (err, result) => {
+    if (err) throw err;
+    console.log('Database ensured...');
+
+    const useDatabase = `USE \`${process.env.DB_DATABASE}\`;`;
+    conn.query(useDatabase, (err, result) => {
+      if (err) throw err;
+
+      const createTable = `
+        CREATE TABLE IF NOT EXISTS product (
+          product_id INT AUTO_INCREMENT PRIMARY KEY,
+          product_name VARCHAR(200),
+          product_price INT
+        ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+      `;
+      conn.query(createTable, (err, result) => {
+        if (err) throw err;
+        console.log('Table ensured...');
+      });
+    });
+  });
 });
 
-//set views file
-app.set('views',path.join(__dirname,'views'));
-//set view engine
+// Set views file
+app.set('views', path.join(__dirname, 'views'));
+// Set view engine
 app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//set folder public as static folder for static file
-app.use('/assets',express.static(__dirname + '/public'));
+// Set folder public as static folder for static file
+app.use('/assets', express.static(__dirname + '/public'));
 var ip = require("ip");
-//route for homepage
-app.get('/',(req, res) => {
+
+// Route for homepage
+app.get('/', (req, res) => {
   let sql = "SELECT * FROM product";
   let query = conn.query(sql, (err, results) => {
-
-    if(err) throw err;
-    res.render('product_view',{
+    if (err) throw err;
+    res.render('product_view', {
       results: results,
       hostIp: ip.address()
     });
   });
 });
 
-//route for insert data
-app.post('/save',(req, res) => {
-  let data = {product_name: req.body.product_name, product_price: req.body.product_price};
+// Route for insert data
+app.post('/save', (req, res) => {
+  let data = { product_name: req.body.product_name, product_price: req.body.product_price };
   let sql = "INSERT INTO product SET ?";
-  let query = conn.query(sql, data,(err, results) => {
-    if(err) throw err;
+  let query = conn.query(sql, data, (err, results) => {
+    if (err) throw err;
     res.redirect('/');
   });
 });
 
-//route for update data
-app.post('/update',(req, res) => {
-  let sql = "UPDATE product SET product_name='"+req.body.product_name+"', product_price='"+req.body.product_price+"' WHERE product_id="+req.body.id;
+// Route for update data
+app.post('/update', (req, res) => {
+  let sql = "UPDATE product SET product_name='" + req.body.product_name + "', product_price='" + req.body.product_price + "' WHERE product_id=" + req.body.id;
   let query = conn.query(sql, (err, results) => {
-    if(err) throw err;
+    if (err) throw err;
     res.redirect('/');
   });
 });
 
-//route for delete data
-app.post('/delete',(req, res) => {
-  let sql = "DELETE FROM product WHERE product_id="+req.body.product_id+"";
+// Route for delete data
+app.post('/delete', (req, res) => {
+  let sql = "DELETE FROM product WHERE product_id=" + req.body.product_id;
   let query = conn.query(sql, (err, results) => {
-    if(err) throw err;
-      res.redirect('/');
+    if (err) throw err;
+    res.redirect('/');
   });
 });
 
-app.get("/ip", function(req, res) {
+app.get("/ip", function (req, res) {
   console.log(req.socket.remoteAddress);
   console.log(req.ip);
   res.send("your IP is: " + req.ip);
 });
 
-//server listening
+// Server listening
 app.listen(8000, () => {
   console.log('Server is running at port 8000');
-  console.log('Your IP is')
+  console.log('Your IP is', ip.address());
 });
